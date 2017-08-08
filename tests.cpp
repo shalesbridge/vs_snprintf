3f7d47ab6e105c82773872e4bc29b6c3be954d6b
Brandon Huber <shalesbridge@users.noreply.github.com>
2017-08-07 23:33:07 -0400

    -Got SingleStringWithInsufficientSpace and SingleStringWithMoreThanEnoughSpace unit tests working for GNU C.

--- tests.cpp
+++ tests.cpp
@@ -19,13 +19,14 @@ TEST(SnprintfTest, SingleStringWithInsufficientSpace)
     int r;
     char dest[] = "ZZZZZZZZZZZZZZZ";
     r = snprintf(dest, 3, "%s", "foo");
-    // EXPECT_STREQ("fo", snprintf());
+    EXPECT_STREQ("fo", dest);
     EXPECT_EQ(3, r);
     //
     // Make sure that snprintf() did not alter anything past where it needed to
     // (being, the position of the null terminator).
-    EXPECT_EQ('Z', dest[3]);
-    EXPECT_STREQ("fooZZZZZZZZZZZZ", dest[3]);
+    EXPECT_EQ('Z', dest[strlen(dest) + 1]);
+    // EXPECT_EQ('Z', dest[3]);
+    // EXPECT_STREQ("fooZZZZZZZZZZZZ", dest);
 }
 
 // Tests putting a single string into sprintf with more than enough space in the
@@ -39,7 +40,7 @@ TEST(SnprintfTest, SingleStringWithMoreThanEnoughSpace)
     //
     // Make sure that snprintf() did not alter anything past where it needed to
     // (being, the position of the null terminator).
-    // EXPECT_EQ('Z', dest[strlen(dest) + 1])
+    EXPECT_EQ('Z', dest[strlen(dest) + 1]);
     EXPECT_STREQ("foo", dest);
 }
 int main(int argc, char *argv[])
